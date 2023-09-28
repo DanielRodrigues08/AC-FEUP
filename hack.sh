7a0e97c278e0dc5b237326b9168dc94d8b16b016
Martim Videira <mitram790@gmail.com>
2023-09-28 20:41:14 +0100

    Vim magiced teste.ipynb

---
+++ hack.sh
@@ -0,0 +1,9 @@
+a
+ywywi=<Esc>i <Esc>pa<Esc>wlipd.read_csv("./data/<Esc>A")<Esc>0xywo<Esc>pa.head(3)<Esc>j0
+b
+ywOimport pandas as pd<Esc>jyyPywoprint(list(<Esc>pa.columns<Esc>Vkk<copy>VjjdO:<BS><Esc>:.! python3 -c '<C-v>'
+jywki = <Esc>0pa_o<BS>columns <Esc>0xjjj
+
+d
+:.! ls ./data
+7@aggjj7@b
