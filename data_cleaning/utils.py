4ecaf176e04d5b5dd916ce778babf9a8950f0474
Daniel Rodrigues <up202006562@up.pt>
2023-10-21 16:09:49 +0100

    data cleaning done

--- data_cleaning/utils.py
+++ data_cleaning/utils.py
@@ -5,19 +5,19 @@ from scipy import stats
 import seaborn as sb
 
 
-def nulls_values_by_column(df):
+def nulls_values_by_column(df, plot_size=(12, 8)):
     null_counts = df.isnull().sum()
-    plot = null_counts.plot(kind="bar", title="Number of null values per column")
+    plot = null_counts.plot(kind="bar", title="Number of null values per column", figsize = plot_size)
     for i, v in enumerate(null_counts):
         plot.text(i, v, str(v), ha="center", va="bottom")
     return plot
 
 
-def unique_values_by_column(df, threshold=0):
+def unique_values_by_column(df, threshold=0, plot_size=(12, 8)):
     counts = {col: len(df[col].unique()) for col in df.columns}
     counts_df = pd.DataFrame.from_dict(counts, orient="index", columns=["count"])
 
-    ax = counts_df.plot(kind="bar", title="Number of Unique Values Per Column")
+    ax = counts_df.plot(kind="bar", title="Number of Unique Values Per Column", figsize = plot_size)
 
     [
         bar.set_color("green") if bar.get_height() > threshold else bar.set_color("red")
@@ -40,13 +40,13 @@ def filter_column_uniques(df, size=1):
     return df_clean
 
 
-def histogram_plot(df, max_zscore=3):
+def histogram_plot(df, max_zscore=3, plot_size=(12, 8)):
     numerical_columns = df.select_dtypes(include=["number"]).columns
 
     num_columns = len(numerical_columns)
     num_rows = (num_columns + 1) // 2
     
-    fig, axes = plt.subplots(num_rows, 2, figsize=(12, 8))
+    fig, axes = plt.subplots(num_rows, 2, figsize=plot_size)
 
     for i, column in enumerate(numerical_columns):
         row = i // 2
@@ -103,13 +103,13 @@ def calculate_bounds_iqr(col, factor=1.5):
     return lower_bound, upper_bound
 
 
-def iqr_plot(df):
+def iqr_plot(df, factor, plot_size=(12, 8)):
     numerical_columns = df.select_dtypes(include=["number"]).columns
 
     num_columns = len(numerical_columns)
     num_rows = (num_columns + 1) // 2
 
-    fig, axes = plt.subplots(num_rows, 2, figsize=(12, 8))
+    fig, axes = plt.subplots(num_rows, 2, figsize=plot_size)
 
     for i, column in enumerate(numerical_columns):
         row = i // 2
@@ -121,7 +121,7 @@ def iqr_plot(df):
             ax = axes[row, col]
         
         sb.boxplot(x=df[column], orient="h", ax=ax)
-        lower_bound, upper_bound = calculate_bounds_iqr(df[column])
+        lower_bound, upper_bound = calculate_bounds_iqr(df[column], factor)
         ax.fill_betweenx(
             ax.get_ylim(),
             lower_bound,
@@ -138,7 +138,7 @@ def iqr_plot(df):
     return plt
 
 
-def scatter_plot(df):
+def scatter_plot(df, plot_size=(15, 10)):
     # Define the number of columns per row
     columns_per_row = 3
     numerical_columns = df.select_dtypes(include=["number"]).columns
@@ -148,7 +148,7 @@ def scatter_plot(df):
     total_rows = (total_plots + columns_per_row - 1) // columns_per_row
 
     # Create the overall figure and axis objects
-    fig, axs = plt.subplots(total_rows, columns_per_row, figsize=(15, 10))
+    fig, axs = plt.subplots(total_rows, columns_per_row, figsize=plot_size)
 
     # Flatten the axis objects to make indexing easier
     axs = axs.flatten()
