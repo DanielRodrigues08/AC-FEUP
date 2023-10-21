de63a92718e0605606ddf4df9afa962b84b3a9a7
Daniel Rodrigues <up202006562@up.pt>
2023-10-21 16:33:05 +0100

    Add script to run Jupyter notebooks as Python scripts

---
+++ run_notebooks.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+import argparse
+
+def main(path):
+    # Loop through each file in the directory
+    for file in os.listdir(path):
+        # Check if the file is a Jupyter notebook
+        if file.endswith('.ipynb'):
+            # Convert the notebook to a Python script using nbconvert
+            subprocess.run(['jupyter', 'nbconvert', '--to', 'script', os.path.join(path, file)])
+            # Run the Python script using subprocess
+            subprocess.run(['python', os.path.join(path, file.replace('.ipynb', '.py'))])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', type=str, default='data_cleaning')
+    args = parser.parse_args()
+    main(path)
