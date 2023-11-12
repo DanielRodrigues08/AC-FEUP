205aa2a0ea48fa85ea7ff95b8d6ed4d0eedfac89
Daniel Rodrigues <up202006562@up.pt>
2023-11-12 18:24:36 +0000

    classification refactored

--- classification/classification_utils.py
+++ classification/classification_utils.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
 from sklearn.model_selection import GridSearchCV, learning_curve
+import pandas as pd
+import copy
 
 
 def split_by_conf(df):
@@ -19,16 +21,18 @@ def split_data(df, year, target):
     return x_train, y_train, x_test, y_test
 
 
-def train_model(model, df, year, target, param_grid):
+def train_model(classifier, df, year, target, param_grid):
+    classifier1 = copy.deepcopy(classifier)  # to create a pure function
     x_train, y_train, _, _ = split_data(df, year, target)
 
-    grid_search = GridSearchCV(model, param_grid, cv=None)
+    grid_search = GridSearchCV(classifier1, param_grid, cv=None)
     grid_search.fit(x_train, y_train)
-    model.set_params(**grid_search.best_params_)
+    classifier1.set_params(**grid_search.best_params_)
 
+    model = copy.deepcopy(classifier1)  # to enable plotting a learning curve with the same parameters
     model.fit(x_train, y_train)
 
-    return model
+    return model, classifier1
 
 
 def test_model(model, df, year, target):
@@ -36,15 +40,35 @@ def test_model(model, df, year, target):
     y_pred = model.predict(x_test)
     y_prob = model.predict_proba(x_test)[:, 1]
 
-    return y_test, y_pred, y_prob
+    return y_test, y_pred, y_prob, x_test['confID']
 
 
-def plot_learning_curve(model, df, year):
-    x_train, y_train, _, _ = split_data(df, year)
-    train_sizes, train_scores, val_scores = learning_curve(model, x_train, y_train, cv=None)
+def plot_learning_curve(classifier, df, year, target):
+    classifier1 = copy.deepcopy(classifier)  # to create a pure function
+    x_train, y_train, _, _ = split_data(df, year, target)
+    train_sizes, train_scores, val_scores = learning_curve(classifier1, x_train, y_train, cv=None)
     plt.plot(train_sizes, train_scores.mean(axis=1), label='Training score')
     plt.plot(train_sizes, val_scores.mean(axis=1), label='Validation score')
     plt.xlabel('Training size')
     plt.ylabel('Accuracy')
     plt.legend(f"Learning Curve for year {year}")
     plt.show()
+
+
+def enforce_max_teams(y_pred, y_prob, conf_id, max_teams=4):
+    joined = zip(range(len(y_pred)), y_prob, conf_id)
+    joined = sorted(joined, key=lambda x: x[1], reverse=True)
+
+    count_0 = 0
+    count_1 = 0
+    for i, _, conf in joined:
+        if count_0 < max_teams and conf == 0:
+            y_pred[i] = 1
+            count_0 += 1
+        elif count_1 < max_teams and conf == 1:
+            y_pred[i] = 1
+            count_1 += 1
+        else:
+            y_pred[i] = 0
+
+    return y_pred
